38b7dad175ebecb6bb6bdf3584357ae9601996b6
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-08-19 13:02:45 +0200

    fix: Fix position metatable

--- server/lua/api/position.lua
+++ server/lua/api/position.lua
@@ -1,14 +1,12 @@
-local position_mt = {
-    __index = {
-        equals = function (self, other)
-            return self.x == other.x and self.y == other.y and self.z == other.z
-        end,
-        tostring = function (self)
-            return string.format("(%d, %d, %d)", self.x, self.y, self.z)
-        end
-    }
+local PositionMT = {
+    __eq = function (self, other)
+        return self.x == other.x and self.y == other.y and self.z == other.z
+    end,
+    __tostring = function (self)
+        return string.format("(%d, %d, %d)", self.x, self.y, self.z)
+    end
 }
 
 function position(x, y, z)
-    return setmetatable({x = x, y = y, z = z}, position_mt)
+    return setmetatable({x = x, y = y, z = z}, PositionMT)
 end
