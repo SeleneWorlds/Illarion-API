cfdaea2252946f23b12de6232dc23e3cf3277a3a
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-08-30 00:17:49 +0200

    feat: Add position.FromSeleneCoordinate to wrap a Coordinate into a proper position with __eq support

--- server/lua/api/position.lua
+++ server/lua/api/position.lua
@@ -1,4 +1,19 @@
-local metatable = {
+position = {}
+
+setmetatable(position, {
+    __call = function (self, x, y, z)
+        return setmetatable({x = x, y = y, z = z}, position.SeleneMetatable)
+    end
+})
+
+position.SeleneMetatable = {
+    __index = function(self, key)
+        local coordinate = rawget(self, "SeleneCoordinate")
+        if coordinate then
+            return coordinate[key]
+        end
+        return rawget(self, key)
+    end,
     __eq = function (self, other)
         return self.x == other.x and self.y == other.y and self.z == other.z
     end,
@@ -7,6 +22,6 @@ local metatable = {
     end
 }
 
-function position(x, y, z)
-    return setmetatable({x = x, y = y, z = z}, metatable)
+function position.FromSeleneCoordinate(coordinate)
+    return setmetatable({SeleneCoordinate = coordinate}, position.SeleneMetatable)
 end
