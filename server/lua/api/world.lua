19d3d1252e84e54870fb38abeca797e6e1e8cd2a
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-08-31 10:44:32 +0200

    feat: Add missing world stubs

--- server/lua/api/world.lua
+++ server/lua/api/world.lua
@@ -3,25 +3,81 @@ local function nyi(name)
 end
 
 world = {}
-world.getTime = nyi("getTime")
-world.getItemStatsFromId = nyi("getItemStatsFromId")
-world.isItemOnField = nyi("isItemOnField")
-world.getItemOnField = nyi("getItemOnField")
-world.changeItem = nyi("changeItem")
-world.getPlayersInRangeOf = nyi("getPlayersInRangeOf")
-world.erase = nyi("erase")
-world.gfx = nyi("gfx")
-world.getPlayersOnline = nyi("getPlayersOnline")
-world.swap = nyi("swap")
-world.makeSound = nyi("makeSound")
-world.getItemName = nyi("getItemName")
-world.getField = nyi("getField")
-world.isCharacterOnField = nyi("isCharacterOnField")
-world.getCharacterOnField = nyi("getCharacterOnField")
-world.createItemFromId = nyi("createItemFromId")
-world.getMonstersInRangeOf = nyi("getMonstersInRangeOf")
-world.getNPCSInRangeOf = nyi("getNPCSInRangeOf")
-world.isPersistentAt = nyi("isPersistentAt")
-world.makePersistentAt = nyi("makePersistentAt")
-world.broadcast = nyi("broadcast")
-world.createMonster = nyi("createMonster")
+
+world.SeleneMethods = {
+    LoS = nyi("LoS"),
+    deleteNPC = nyi("deleteNPC"),
+    createDynamicNPC = nyi("createDynamicNPC"),
+    getPlayersOnline = nyi("getPlayersOnline"),
+    getPlayerIdByName = nyi("getPlayerIdByName"),
+    getNPCS = nyi("getNPCS"),
+    getCharactersInRangeOf = nyi("getCharactersInRangeOf"),
+    getPlayersInRangeOf = nyi("getPlayersInRangeOf"),
+    getMonstersInRangeOf = nyi("getMonstersInRangeOf"),
+    getNPCSInRangeOf = nyi("getNPCSInRangeOf"),
+    getArmorStruct = nyi("getArmorStruct"),
+    getWeaponStruct = nyi("getWeaponStruct"),
+    getNaturalArmor = nyi("getNaturalArmor"),
+    getMonsterAttack = nyi("getMonsterAttack"),
+    changeQuality = nyi("changeQuality"),
+    changeItem = nyi("changeItem"),
+    isCharacterOnField = nyi("isCharacterOnField"),
+    getCharacterOnField = nyi("getCharacterOnField"),
+    getField = nyi("getField"),
+    makePersistentAt = nyi("makePersistentAt"),
+    removePersistenceAt = nyi("removePersistenceAt"),
+    isPersistentAt = nyi("isPersistentAt"),
+    getTime = nyi("getTime"),
+    erase = nyi("erase"),
+    increase = nyi("increase"),
+    swap = nyi("swap"),
+    createItemFromId = nyi("createItemFromId"),
+    createItemFromItem = nyi("createItemFromItem"),
+    createMonster = nyi("createMonster"),
+    gfx = nyi("gfx"),
+    makeSound = nyi("makeSound"),
+    getItemStats = nyi("getItemStats"),
+    getItemStatsFromId = nyi("getItemStatsFromId"),
+    setWeather = nyi("setWeather"),
+    isItemOnField = nyi("isItemOnField"),
+    getItemOnField = nyi("getItemOnField"),
+    changeTile = nyi("changeTile"),
+    getItemName = nyi("getItemName"),
+    createSavedArea = nyi("createSavedArea"),
+    broadcast = nyi("broadcast"),
+    sendMonitoringMessage = nyi("sendMonitoringMessage")
+}
+
+world.SeleneGetters = {
+    weather = nyi("weather")
+}
+
+world.SeleneSetters = {
+    weather = nyi("weather")
+}
+
+world.SeleneMetatable = {
+    __index = function(table, key)
+        local method = world.SeleneMethods[key]
+        if method then
+            return method
+        end
+        local getter = world.SeleneGetters[key]
+        if getter then
+            return getter(table)
+        end
+
+        return rawget(table, key)
+    end,
+    __newindex = function(table, key, value)
+        local setter = world.SeleneSetters[key]
+        if setter then
+            setter(table, value)
+            return
+        end
+
+        rawset(table, key, value)
+    end
+}
+
+setmetatable(world, world.SeleneMetatable)
