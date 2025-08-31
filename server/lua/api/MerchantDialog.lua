7fb19b42a39016ba08dd846fa42490c05b5fb362
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-08-31 10:34:48 +0200

    feat: Add MerchantDialog stubs

---
+++ server/lua/api/MerchantDialog.lua
@@ -0,0 +1,51 @@
+local function nyi(name)
+    return function() error(name .. " not yet implemented") end
+end
+
+MerchantDialog = {}
+
+MerchantDialog.SeleneMethods = {
+    addOffer = nyi("addOffer"),
+    addPrimaryRequest = nyi("getData"),
+    addSecondaryRequest = nyi("addSecondaryRequest"),
+    getResult = nyi("getResult"),
+    getPurchaseIndex = nyi("getPurchaseIndex"),
+    getPurchaseAmount = nyi("getPurchaseAmount"),
+    getSaleItem = nyi("getSaleItem"),
+    getLookAtList = nyi("getLookAtList"),
+}
+
+MerchantDialog.SeleneGetters = {}
+MerchantDialog.SeleneSetters = {}
+
+MerchantDialog.SeleneMetatable = {
+    __call = function(self, title, callback)
+        local o = {
+            title = title,
+            callback = callback
+        }
+        setmetatable(o, self)
+        self.__index = self
+    end,
+    __index = function(table, key)
+        local method = MerchantDialog.SeleneMethods[key]
+        if method then
+            return method
+        end
+        local getter = MerchantDialog.SeleneGetters[key]
+        if getter then
+            return getter(table)
+        end
+
+        return rawget(table, key)
+    end,
+    __newindex = function(table, key, value)
+        local setter = MerchantDialog.SeleneSetters[key]
+        if setter then
+            setter(table, value)
+            return
+        end
+
+        rawset(table, key, value)
+    end
+}
